44be2814d65a294fda4927d8bb6d730f397302cf
Ricky Fernando <rickyfernandoacong@gmail.com>
2019-11-06 00:56:18 +0700

    done making the first input interface, working on switching frame

--- gui2.py
+++ gui2.py
@@ -1,8 +1,14 @@
 import tkinter as tk
+# import fecesrecognition as fr
 from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
+from tkinter import messagebox
 
+methods=[
+    ("Euclidean Distance",1),
+    ("Cosine distance",2)
+]
 class Window(Tk):
     def __init__(self):
         super(Window, self).__init__()
@@ -13,21 +19,36 @@ class Window(Tk):
         self.labelFrame=ttk.LabelFrame(self,text = "Open an Image")
         self.labelFrame.grid(row=0,column=0)
 
-        self.inputNumber()
-        self.buttonSearch()
-        self.radButton1()
-        self.buttonSubmit()
-
-    def inputNumber(self):
-        self.inputNumber=ttk.Label(self.labelFrame,text="Number of matches")
-        self.inputNumber.grid(row=3,column=0)
-        n=Entry(self.labelFrame,width=10)
-        n.grid(row=3,column=1)
-        self.n=n.get()
+        # inputing number of matches 
+        self.entry1=Entry(self.labelFrame)
+        self.entry1.insert(0,"Put number of expected matches here")
+        self.entry1.grid(row=3,column=0)
         
+        # radio button for method
+        self.method=IntVar()
+        self.method.set(1)
+        for val,choice in enumerate(methods):
+            b=Radiobutton(self.labelFrame,text=choice[0],variable=self.method,value=choice[1])
+            b.grid(row=5,column=val)
+
+        #submit button
+        self.buttonSubmit=ttk.Button(self.labelFrame,text="Send",command=self.onClicked)
+        self.buttonSubmit.grid(row=6,column=0)
+
+        self.buttonSearch()
+
+    def onClicked(self):
+        s1=self.entry1.get()
+        s2=self.method.get()
+        if(s1.isdigit()):
+            s1=int(s1)
+            print(s1," ",s2," ",self.filename)
+        else:
+            messagebox.showerror("Error","Number must be an integer! please re-input")
+
     def buttonSearch(self):
         self.buttonSearch=ttk.Button(self.labelFrame,text= "Browse an Image",command=self.fileDialog)
-        self.buttonSearch.grid(row=1,column=0,columnspan=2)        
+        self.buttonSearch.grid(row=1,column=0)        
 
     def fileDialog(self):
         self.filename= filedialog.askopenfilename(initialdir="Data\DataUji",title = "Select an Image",filetype=(("jpg","*.jpg"),("All File","*,*")))
@@ -35,27 +56,6 @@ class Window(Tk):
         self.label.grid(row=2,column=0,columnspan=2)
         self.label.configure(text=self.filename)
         
-   
-    def radButton1(self):
-        global v
-        v=IntVar()
-        v.set(1)
-        self.label=ttk.Label(self.labelFrame,text="Choose your method:")
-        self.label.grid(row=4,column=0)
-        self.radButton2=ttk.Radiobutton(self.labelFrame,text="Euclidean Distance",variable=v,value=1,command= self.v<-v.get())
-        self.radButton2.grid(row=5,column=0)
-        self.radButton2=ttk.Radiobutton(self.labelFrame,text="Cosine distance",variable=v,value=2,command=self.v<-v.get())
-        self.radButton2.grid(row=5,column=1)
-
-    def show(self):
-        x=str(self.n)+" "+str(self.v)+" "+self.filename
-        self.label=ttk.Label(self.labelFrame,text=x)
-        self.label.grid(row=7,column=0)
-        print(self.n," ",self.v," ",self.filename)    
-    
-    def buttonSubmit(self):
-        self.buttonSubmit=ttk.Button(self.labelFrame,text= "Send",command=self.show())
-        self.buttonSubmit.grid(row=6,column=0)        
 
 
 if __name__=='__main__':
