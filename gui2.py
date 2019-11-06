320f67639971174f164f454fcf8aabee3390b5b8
Ricky Fernando <rickyfernandoacong@gmail.com>
2019-11-06 14:02:35 +0700

    to change branch

--- gui2.py
+++ gui2.py
@@ -13,87 +13,79 @@ methods=[
 class Window(Tk):
     def __init__(self):
         super(Window, self).__init__()
-        self.n=0
-        self.v=0
-        self.filename=""
-        self.title("Input Gui")
-        self.labelFrame=ttk.LabelFrame(self,text = "Open an Image")
-        self.labelFrame.grid(row=0,column=0)
 
-        # inputing number of matches 
-        self.entry1=Entry(self.labelFrame)
-        self.entry1.insert(0,"Put number of expected matches here")
-        self.entry1.grid(row=3,column=0)
+        self.title("Input Gui")
+        
+        
+        self.labelFrame=ttk.LabelFrame(self,text = "Open an Image")
+        self.labelFrame.grid(row=0,column=0,sticky=N+S+W+E)
+        
+        #browse picture
+        self.buttonSearch()
         
         # radio button for method
         self.method=IntVar()
         self.method.set(1)
+        self.radioLabel=ttk.Label(self.labelFrame,text="Choose your method : ")
+        self.radioLabel.grid(row=3,columnspan=6)
         for val,choice in enumerate(methods):
             b=Radiobutton(self.labelFrame,text=choice[0],variable=self.method,value=choice[1])
-            b.grid(row=5,column=val)
+            b.grid(row=4,column=val*3,columnspan=3)
 
         #submit button
         self.buttonSubmit=ttk.Button(self.labelFrame,text="Send",command=self.onClicked)
-        self.buttonSubmit.grid(row=6,column=0)
-
-        self.buttonSearch()
-        #self.showResult()
-    def onClicked(self):
-        s1=self.entry1.get()
-        s2=self.method.get()
-        if(s1.isdigit()):
-            s1=int(s1)
-            self.labelFrame.grid_forget()
-            # self.photos,self.names,self.matches=fr.run(s1,s2,self.filename)
-            
-            # img1= ImageTk.PhotoImage(Image.open("Data\DataUji\pins_Aaron PaulTest\Aaron Paul2_293.jpg"))
-            # img2= ImageTk.PhotoImage(Image.open("Data\DataUji\pins_Amaury NolascoTest\Amaury Nolasco6.jpg"))
-            # img3= ImageTk.PhotoImage(Image.open("Data\DataUji\pins_Alvaro MorteTest\Alvaro Morte16_879.jpg"))
-
-            # self.photos=[img1,img2,img3]
-            # self.names=["a","b","c"]
-            # self.matches=[90,80,70]
-
-            self.showResult(1)
-        else:
-            messagebox.showerror("Error","Number must be an integer! please re-input")
+        self.buttonSubmit.grid(row=5,column=0,columnspan=6)
 
     def buttonSearch(self):
         self.buttonSearch=ttk.Button(self.labelFrame,text= "Browse an Image",command=self.fileDialog)
-        self.buttonSearch.grid(row=1,column=0)        
+        self.buttonSearch.grid(row=1,column=0,columnspan=6)
 
     def fileDialog(self):
         self.filename= filedialog.askopenfilename(initialdir="Data\DataUji",title = "Select an Image",filetype=(("jpg","*.jpg"),("All File","*,*")))
         self.label=ttk.Label(self.labelFrame,text="")
-        self.label.grid(row=2,column=0,columnspan=2)
+        self.label.grid(row=2,column=0,columnspan=6)
         self.label.configure(text=self.filename)
+        
+
+    def onClicked(self):
+        s2=self.method.get()
+        self.labelFrame.pack_forget()
+        # self.photos,self.names,self.matches=fr.run(s2,self.filename)
+        
+        img1= ImageTk.PhotoImage(Image.open("Data\DataUji\pins_Aaron PaulTest\Aaron Paul2_293.jpg"))
+        img2= ImageTk.PhotoImage(Image.open("Data\DataUji\pins_Amaury NolascoTest\Amaury Nolasco6.jpg"))
+        img3= ImageTk.PhotoImage(Image.open("Data\DataUji\pins_Alvaro MorteTest\Alvaro Morte16_879.jpg"))
+
+        self.photos=[img1,img2,img3]
+        
+        self.showResult(1)
         
     def showResult(self,imageNth):
         
-        self.label2=ttk.Label(self,image=self.photos[imageNth-1])
-        self.label2.grid(row=0,column=0,columnspan=3)
+        self.label2=Label(self,image=self.photos[imageNth-1],bg='black')
+        self.label2.grid(row=0,column=0,columnspan=3,sticky=N+S+W+E)
 
-        self.textLabel=ttk.Label(self,text="Nama : "+self.names[imageNth-1])
-        self.textLabel.grid(row=1,column=0)
-        self.textLabel2=ttk.Label(self,text="Persentase Kemiripan : "+str(self.matches[imageNth-1]))
-        self.textLabel2.grid(row=2,column=0,columnspan=2)
+        self.textLabel=Label(self,text="Ranking : "+str(imageNth),bg='black',fg='white')
+        self.textLabel.grid(row=1,column=0,columnspan=3,sticky=N+S+W+E)
         
         if(imageNth== len(self.photos)):
-            self.buttonNext=ttk.Button(self,text="-->",command=lambda:self.showResult(1))
-            self.buttonBack=ttk.Button(self,text="<--",command=lambda:self.showResult(imageNth-1))
+            self.buttonNext=Button(self ,text="-->",bg='black',fg='white',command=lambda:self.showResult(1))
+            self.buttonBack=Button(self ,text="<--",bg='black',fg='white',command=lambda:self.showResult(imageNth-1))
         else: 
             if(imageNth == 1):
-                self.buttonNext=ttk.Button(self,text="-->",command=lambda:self.showResult(imageNth+1))
-                self.buttonBack=ttk.Button(self,text="<--",command=lambda:self.showResult(len(self.photos)))
+                self.buttonNext=Button(self ,text="-->",bg='black',fg='white',command=lambda:self.showResult(imageNth+1))
+                self.buttonBack=Button(self ,text="<--",bg='black',fg='white',command=lambda:self.showResult(len(self.photos)))
             else:
-                self.buttonNext=ttk.Button(self,text="-->",command=lambda:self.showResult(imageNth+1))
-                self.buttonBack=ttk.Button(self,text="<--",command=lambda:self.showResult(imageNth-1))
+                self.buttonNext=Button(self ,text="-->",bg='black',fg='white',command=lambda:self.showResult(imageNth+1))
+                self.buttonBack=Button(self ,text="<--",bg='black',fg='white',command=lambda:self.showResult(imageNth-1))
         
-        self.buttonExit=ttk.Button(self,text= "Exit",command=self.quit)
-        self.buttonBack.grid(row=3,column=0)
-        self.buttonExit.grid(row=3,column=1)
-        self.buttonNext.grid(row=3,column=2)
+        self.buttonExit=Button(self ,text= "Exit",bg='black',fg='white',command=self.quit)    
+        self.buttonBack.grid(row=2,column=0,sticky=N+S+W+E)
+        self.buttonExit.grid(row=2,column=1,sticky=N+S+W+E)
+        self.buttonNext.grid(row=2,column=2,sticky=N+S+W+E)
      
 if __name__=='__main__':
     window = Window()
+    Grid.rowconfigure(window,0)
+    Grid.columnconfigure(window,0)
     window.mainloop()
