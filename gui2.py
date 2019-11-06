ac80a33be1e1d582ea64b7acbcf79bb05850780e
unknown <muha.kamal@gmail.com>
2019-11-06 22:45:54 +0700

    progress and loop

--- gui2.py
+++ gui2.py
@@ -1,4 +1,3 @@
-import cv2
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk
@@ -16,13 +15,18 @@ class Window(Tk):
         super(Window, self).__init__()
 
         self.title("Input Gui")
-        
-        
+        self.build()
+
+    def build(self):
+        self.filename=None
+        self.spinLabel=None
+
         self.labelFrame=ttk.LabelFrame(self,text = "Open an Image")
         self.labelFrame.grid(row=0,column=0,sticky=N+S+W+E)
         
         #browse picture
-        self.buttonSearch()
+        self.buttonSearch=ttk.Button(self.labelFrame,text= "Browse an Image",command=self.fileDialog)
+        self.buttonSearch.grid(row=1,column=0,columnspan=6)
         
         # radio button for method
         self.method=IntVar()
@@ -34,18 +38,15 @@ class Window(Tk):
             b.grid(row=4,column=val*3,columnspan=3)
 
         # spin box for how many matches
-        self.spinText=ttk.Label(self.labelFrame, text = "Number of matches : ")
-        self.spinText.grid(row = 5, column = 0, columnspan = 3)
-        self.spinLabel=ttk.Spinbox(self.labelFrame, from_= 1, to = 20, width = 4)
-        self.spinLabel.grid(row = 5, column = 3, columnspan = 3)
-
+        self.spinText=ttk.Label(self.labelFrame,text="Number of matches : ")
+        self.spinText.grid(row=5,column=0,columnspan=3)
+        self.spinLabel=ttk.Spinbox(self.labelFrame,from_=1,to=20,width=4)
+        self.spinLabel.grid(row=5,column=3,columnspan=3)
+        
         #submit button
         self.buttonSubmit=ttk.Button(self.labelFrame,text="Send",command=self.onClicked)
         self.buttonSubmit.grid(row=6,column=0,columnspan=6)
 
-    def buttonSearch(self):
-        self.buttonSearch=ttk.Button(self.labelFrame,text= "Browse an Image",command=self.fileDialog)
-        self.buttonSearch.grid(row=1,column=0,columnspan=6)
 
     def fileDialog(self):
         self.filename= filedialog.askopenfilename(initialdir="Data\DataUji",title = "Select an Image",filetype=(("jpg","*.jpg"),("All File","*,*")))
@@ -55,15 +56,17 @@ class Window(Tk):
         
 
     def onClicked(self):
-        s2=self.method.get()
-        self.labelFrame.pack_forget()
+        if(self.filename is not None and self.spinLabel.get().isdigit()):
+            s2=self.method.get()
+            self.labelFrame.pack_forget()
 
-        temp,self.matches,temp2=fr.find_match(self.filename, s2, int(self.spinLabel.get()))
-        
-        self.photos = [ImageTk.PhotoImage(Image.fromarray(foto,'RGB')) for foto in temp]
-        self.tested = ImageTk.PhotoImage(Image.fromarray(temp2,'RGB'))
-        self.showResult(1)
-        
+            temp,self.matches,temp2=fr.find_match(self.filename, s2, int(self.spinLabel.get()))
+            
+            self.photos = [ImageTk.PhotoImage(Image.fromarray(foto,'RGB')) for foto in temp]
+            self.tested = ImageTk.PhotoImage(Image.fromarray(temp2,'RGB'))
+            self.showResult(1)
+        else:
+            messagebox.showerror('Error','Please re-check your input')        
     def showResult(self,imageNth):
         
         self.label1=Label(self,image=self.tested,bg='black')
@@ -86,11 +89,19 @@ class Window(Tk):
                 self.buttonNext=Button(self ,text="Next",bg='black',fg='white',command=lambda:self.showResult(imageNth+1))
                 self.buttonBack=Button(self ,text="Prev",bg='black',fg='white',command=lambda:self.showResult(imageNth-1))
         
-        self.buttonExit=Button(self ,text= "Exit",bg='black',fg='white',command=self.quit)    
+        self.buttonExit=Button(self ,text= "Exit",bg='black',fg='white',command=lambda:self.forget())    
         self.buttonBack.grid(row=2,column=0,columnspan=2,sticky=N+S+W+E)
         self.buttonExit.grid(row=3,column=0,columnspan=2,sticky=N+S+W+E)
         self.buttonNext.grid(row=4,column=0,columnspan=2,sticky=N+S+W+E)
      
+    def forget(self):
+         self.label1.grid_forget()
+         self.label2.grid_forget()
+         self.textLabel.grid_forget()
+         self.buttonBack.grid_forget()
+         self.buttonExit.grid_forget()
+         self.buttonNext.grid_forget()
+         self.build()
 if __name__=='__main__':
     window = Window()
     Grid.rowconfigure(window,0,weight=1)
